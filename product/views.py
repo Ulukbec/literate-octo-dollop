96efcf608d13f76b74f4d2ca2066fa99c376130a
Ulukbec <114999981+Ulukbec@users.noreply.github.com>
2023-01-30 15:47:04 +0600

    Update views.py

--- product/views.py
+++ product/views.py
@@ -8,7 +8,7 @@ def main_view(request):
     return render(request, 'layouts/index.html')
 
 
-def products_view(request):
+def products_viewssdfffsdf(request):
     if request.method == "GET":
         products = Product.objects.all()
         data = {
